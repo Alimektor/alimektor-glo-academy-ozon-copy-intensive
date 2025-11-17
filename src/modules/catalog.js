ba73aed7d20a62e8780aa90866bbc654f8c3cf40
アリメクタ アレネクト <alimektor@gmail.com>
2025-11-17 23:24:42 +0300

    feat: Add price filtering functionality to catalog module
    - Add min/max price input fields handling.
    - Implement priceFilter function in filters module.
    - Create renderPrice function to handle price filtering logic.
    - Add event listeners for price input changes.

--- src/modules/catalog.js
+++ src/modules/catalog.js
@@ -1,19 +1,28 @@
 import getData from "./getData.js";
 import renderGoods from "./renderGoods.js";
-import { categoryFilter } from "./filters.js";
+import { categoryFilter, priceFilter } from "./filters.js";
 
 const catalog = () => {
     const btnCatalog = document.querySelector('.catalog-button');
     const catalogModal = document.querySelector('.catalog');
     const catalogModalItems = document.querySelectorAll('.catalog li');
-
-    console.log(catalogModalItems);
+    const minPriceInput = document.querySelector('#min.filter-price_input');
+    const maxPriceInput = document.querySelector('#max.filter-price_input');
 
     let isOpen = false;
 
+    async function renderPrice() {
+        getData().then(data => {
+            const minPrice = minPriceInput.value;
+            const maxPrice = maxPriceInput.value;
+            const filteredData = priceFilter(data, minPrice, maxPrice);
+            renderGoods(filteredData);
+        });
+    }
+
     btnCatalog.addEventListener('click', () => {
-        isOpen = !isOpen;
         if (isOpen) {
+        isOpen = !isOpen;
             catalogModal.style.display = '';
             return;
         }
@@ -28,6 +37,13 @@ const catalog = () => {
                 renderGoods(filteredData);
             });
         });
+    });
+
+    minPriceInput.addEventListener('input', () => {
+        renderPrice();
+    });
+    maxPriceInput.addEventListener('input', () => {
+        renderPrice();
     });
 }
 
--- src/modules/filters.js
+++ src/modules/filters.js
@@ -5,3 +5,12 @@ export const searchFilter = (goods, value) => {
 export const categoryFilter = (goods, value) => {
     return goods.filter(goodItem => goodItem.category === value);
 }
+
+export const priceFilter = (goods, minValue, maxValue) => {
+    const minPrice = Number(minValue) || 0;
+    const maxPrice = Number(maxValue) || 0;
+    return goods.filter(goodItem => {
+        const itemPrice = Number(goodItem.price);
+        return itemPrice >= minPrice && itemPrice <= maxPrice;
+    });
+}
