2e911b3046414f3b3380c81ebe4cb39607996b2e
アリメクタ アレネクト <alimektor@gmail.com>
2025-11-17 18:36:40 +0300

    feat: Create postData.js / getData.js module

---
+++ src/modules/getData.js
@@ -0,0 +1,6 @@
+const getData = () => {
+    return fetch('https://test-c7ae2-default-rtdb.europe-west1.firebasedatabase.app/goods.json')
+        .then(response => response.json())
+}
+
+export default getData;
